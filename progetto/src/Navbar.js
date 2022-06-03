64c2a0b7d432aa618461d34d17e8b4d551077c88
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-03 16:20:15 +0200

    header

---
+++ progetto/src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Row>
+        <Col span={12}>col</Col>
+        <Col span={12}>col</Col>
+      </Row>
+    </nav>
+  );
+}
