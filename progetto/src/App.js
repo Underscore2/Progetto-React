0cf9b486723f25eeab749ba3fe5ed1c0d89acac3
Gabriel <Gabrielteddelavoro@gmail.com>
2022-06-06 12:22:42 +0200

    feat: Bozza navbar

--- progetto/src/App.js
+++ progetto/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import Navbar from './Navbar.js';
+import './css/helper-class.css';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-     
+    <div className='App bg-black ' >
+
       <header>
-        <Navbar/>
+        <Navbar />
       </header>
 
     </div>
--- progetto/src/Navbar.js
+++ progetto/src/Navbar.js
@@ -1,13 +1,63 @@
-import React from "react";
-import { Row, Col } from "antd";
+import React, { useState } from 'react';
+import { Row, Col, Space, Typography } from 'antd';
+import './utilities/texts.json';
+import './css/helper-class.css';
+import logo from './images/egg-fill.svg';
+import Modal from 'antd/lib/modal/Modal';
 
-export default function Navbar() {
+
+
+function Navbar() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  }
   return (
-    <nav>
-      <Row>
-        <Col span={12}>col</Col>
-        <Col span={12}>col</Col>
-      </Row>
-    </nav>
-  );
+    < >
+      <div className='p-1'>
+        <Row className='white-modules d-flex align-center px-5' style={{ height: '7vh' }}>
+          <Col span={6} >
+            <div>
+              <img src={logo} style={{ width: '50px', height: 'auto' }} alt='logo'></img>
+            </div>
+          </Col>
+          <Col span={6} offset={12} className="d-flex justify-end w-100" >
+            <ul className='list-style d-flex p-0 m-0 align-center'>
+              <Space size={15} className="d-flex align-center">
+                <li>
+                  <a href='#' className='text-decoration font-primary'>Features</a>
+                </li>
+                <li>
+                  <a href='#' className='text-decoration font-primary'>Pricing</a>
+                </li>
+                <li>
+                  <a href='#' className='text-decoration font-primary' onClick={showModal}>Login</a>
+                </li>
+
+                <li>
+                  <a href='#' className='text-decoration font-primary'>Sign Up</a>
+                </li>
+              </Space>
+            </ul>
+          </Col>
+          <Modal className='white-modules bg-black' title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
+        </Row>
+      </div>
+    </>
+  )
 }
+
+export default Navbar;
