f2091dcc656d7474feee7adb0cb8ecc5ecd72d85
Gabriel <Gabrielteddelavoro@gmail.com>
2022-06-08 13:01:12 +0200

    feat: Hero done

--- progetto/src/App.js
+++ progetto/src/App.js
@@ -1,11 +1,13 @@
-import Homepage from './Components/Homepage';
-import './css/helper-class.css'
+import Hero from './Components/Hero.js';
 import './App.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './css/helper-class.css'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div className="App bg-black">
+      <Hero />
     </div>
   );
 }
