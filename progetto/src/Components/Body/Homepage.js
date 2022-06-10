660d905ac94ea0b5a32d3ec46939a2608184aaae
Aurelio Z <whytemagic@gmail.com>
2022-06-10 02:56:30 +0200

    feat: Implementato routing

--- progetto/src/Components/Body/Homepage.js
+++ progetto/src/Components/Body/Homepage.js
@@ -4,15 +4,19 @@ import Graphic from "./Graphic";
 import List from "./List";
 import Description from "./Description";
 import Cards from "./Cards";
+import Footer from "../Footer/Footer";
+import Navbar from "../Header/Navbar";
 
 export default function Homepage() {
   return (
     <>
+        {/*<Navbar/>*/}
         <Hero />
         <Description/>
         <Graphic/>
         <List/>
         <Cards/>
+        {/*<Footer/>*/}
     </>
   )
 }
--- progetto/src/Components/Header/Navbar.js
+++ progetto/src/Components/Header/Navbar.js
@@ -1,66 +1,36 @@
-import text from "../../utilities/texts.json";
-import logo from "../../images/egg-fill.svg";
-import { Row, Col } from "react-bootstrap";
-import menu from "../../images/cell.svg";
+import {Row, Col, Button} from "react-bootstrap";
 import React from "react";
+import {Link, Outlet} from "react-router-dom";
+import logo from "../../images/egg-fill.svg";
 
-class Navbar extends React.Component {
-  render() {
+export default function Navbar(){
     return (
       <div className="container-xxl p-3">
         <Row className="m-0 py-3 white-modules justify-content-center align-items-center px-2 m-0 w-100">
-          <Col
-            className="p-1 align-items-center justify-content-around flex-sm-row"
-            xs={9}
-            sm={9}
-            md={7}
-          >
-            <img
-              src={logo}
-              className="uovo"
-              style={{ width: "50px", height: "auto" }}
-              alt="logo"
-            ></img>
-            <a href="!#" className="text-decoration-none font-secondary ">
+          <Col className="p-1 align-items-center justify-content-around flex-sm-row" xs={9} sm={9} md={7}>
+            <img src={logo} className="uovo" style={{ width: "50px", height: "auto" }} alt="logo" />
+            <Link className="text-decoration-none font-secondary " to={"/"}>
               LMAO Corporation
-            </a>
+            </Link>
           </Col>
-          <Col
-            xs={3}
-            sm={3}
-            md={5}
-            className="d-md-flex p-1 justify-content-end align-items-center p-0 m-0"
-          >
+          <Col xs={3} sm={3} md={5} className="d-md-flex p-1 justify-content-end align-items-center p-0 m-0">
             <ul className="gx-5 list-unstyled d-md-flex d-none justify-content-sm-end p-0 m-0 gap:5">
-              {text.map((item) => {
-                return item.Navbar.map((link, index) => {
-                  return (
-                    <li key={index}>
-                      <a
-                        className="text-decoration-none px-2 font-secondary"
-                        href="!#"
-                      >
-                        {link}
-                      </a>
-                    </li>
-                  );
-                });
-              })}
+              <li>
+                      <Link className="text-decoration-none px-2 font-secondary" to={"/"}>Home</Link>
+              </li>
+              <li>
+                    <Link className="text-decoration-none px-2 font-secondary" to={"/login"}>Login</Link>
+              </li>
+              <li>
+                    <Link className="text-decoration-none px-2 font-secondary" to={"signup"}>Sign Up</Link>
+              </li>
             </ul>
-            <div className="d-flex justify-content-center align-items-center d-md-none">
-              <a href="">
-                <img
-                  src={menu}
-                  className="menu"
-                  style={{ width: "40px", height: "auto" }}
-                ></img>
-              </a>
-            </div>
+
           </Col>
         </Row>
+          <Outlet/>
       </div>
-    );
-  }
-}
 
-export default Navbar;
+    )
+
+  }
